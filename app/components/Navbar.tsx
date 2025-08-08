4575a286902cfd4d92fe3c8382bd97c49a17e5d1
Anirudh M <animadan2005@gmail.com>
2025-08-08 20:19:13 +0530

    logout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,25 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
-            </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
-        </nav>
-    )
-}
-export default Navbar
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+      <div className="flex justify-end p-4">
+        <Link
+          to="https://resume-ats-8fe79.web.app/"
+          className="primary-button w-fit text-base font-medium"
+          rel="noopener noreferrer"
+        >
+          Logout
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
